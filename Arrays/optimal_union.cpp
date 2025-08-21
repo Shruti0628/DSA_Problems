708271258fa5e6d3238555b519aa007f4dbde623
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-08-21 12:06:20 +0530

    Optimal solution for union

---
+++ Arrays/optimal_union.cpp
@@ -0,0 +1,60 @@
+//Union of two sets with an optimal approach
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>optimal_union(vector<int>&arr, vector<int>&arr1){
+    vector<int>res;
+    int s1 = arr.size();
+    int s2 = arr1.size();
+    int i=0, j=0;
+    while(i<s1 && j<s2){
+        if(arr[i]<arr1[j]){
+            if(res.empty() || res.back() != arr[i]){
+                res.push_back(arr[i]);
+            }
+            i++;
+        }
+        else{
+            if(res.empty() || res.back() != arr1[j]){
+                res.push_back(arr1[j]);
+            }
+            j++;
+        }
+    }
+    if(i<s1){
+        if(res.empty() || res.back()!= arr[i]){
+            res.push_back(arr[i]);
+        }
+        i++;
+    }
+    if(j<s2){
+        if(res.empty() || res.back()!=arr1[j]){
+            res.push_back(arr1[j]);
+        }
+        j++;
+    }
+    return res;
+}
+int main(){
+    int n;
+    int n1;
+    cout<<"Enter the size of the first array:";
+    cin>>n;
+    cout<<"Enter the size of the second array:";
+    cin>>n1;
+    vector<int> arr(n);
+    vector<int> arr1(n1);
+    cout<<"Enter the elements of the first array:";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter the elements of the second array:";
+    for(int i=0; i<n1; i++){
+        cin>>arr1[i];
+    }
+    vector<int>res = optimal_union(arr, arr1);
+    cout<<"Union of the two arrays are:"<<endl;
+    for(int i=0; i<res.size(); i++){
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
